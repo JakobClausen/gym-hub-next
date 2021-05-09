ce1b401e017d9c9819909e74ff4ed355f23fd981
Jakob Clausen <jakob.fridmar@gmail.com>
2021-05-09 20:54:13 +0200

    add inputs and submit btn

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,14 +1,7 @@
 import { Formik } from "formik";
-import styled from "styled-components";
-import { Input } from "../styles/styledComponents/form";
+import { Form, Input, SubmitButton } from "../styles/styledComponents/form";
 
 interface LoginFormProps {}
-const Container = styled.div`
-  background: #000000;
-  width: 400px;
-  height: 400px;
-  padding: 20px;
-`;
 export const LoginForm: React.FC<LoginFormProps> = ({}) => {
   return (
     <Formik
@@ -17,9 +10,27 @@ export const LoginForm: React.FC<LoginFormProps> = ({}) => {
         console.log(values);
       }}
     >
-      <Container>
-        <Input />
-      </Container>
+      {({ handleSubmit, handleChange, handleBlur, values, isSubmitting }) => (
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Input
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <SubmitButton type="submit" disabled={isSubmitting}>
+            Login
+          </SubmitButton>
+        </Form>
+      )}
     </Formik>
   );
 };
--- src/styles/styledComponents/form.ts
+++ src/styles/styledComponents/form.ts
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
-export const Input = styled.input`
-  width: 100%;
+export const Form = styled.form`
+  padding: 20px;
+  background: #888888;
 `;
+
+export const Input = styled.input`
+  display: block;
+`;
+
+export const SubmitButton = styled.button``;
